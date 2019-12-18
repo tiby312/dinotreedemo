90057a3e89bc7e99de1089d3a139aff02a466449
Ken <kenakioreed@gmail.com>
2019-12-17 20:26:12 -0500

    trying to get border to show correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,11 @@
 use crate::bot::*;
 
-use ordered_float::*;
+
 use axgeom::*;
 use axgeom;
 use dinotree_alg::prelude::*;
 use duckduckgeo::*;
 
-
-//input:
-//a minimum rectangle that must be visible in the game world
-//the window dimensions.
-//output:
-//the game world dimensions
-pub fn compute_border(rect:Rect<f32>,window:Vec2<f32>)->Rect<f32>{
-    
-    println!("game word minimum={:?}",rect);
-    println!("window={:?}",window);
-    let target_aspect_ratio=window.x/window.y;
-
-
-    let ((x1,x2),(y1,y2))=rect.get();
-    let w=x2-x1;
-    let h=y2-y1;
-
-    let current_aspect_ratio=w/h;
-
-    let [xx,yy]=if target_aspect_ratio<current_aspect_ratio{
-        //target is thinner
-        [0.0,-h+(window.y*w)/window.x]
-
-    }else{
-        //target is wider
-        [window.x*h/window.y-w,0.0]
-    };
-
-    let xx_half=xx/2.0;
-    let yy_half=yy/2.0;
-
-    let xx1=x1-xx_half;
-    let xx2=x2+xx_half;
-
-    let yy1=y1-yy_half;
-    let yy2=y2+yy_half;
-
-    let r=Rect::new(xx1,xx2,yy1,yy2);
-    println!("game world target={:?}",r);
-    r
-}
 
 
 pub struct BotSystem {
@@ -59,7 +18,8 @@ pub struct BotSystem {
 impl BotSystem{
 
     pub fn new(aspect_ratio:f64,num_bots:usize) -> (BotSystem,Rect<f32>,f32) {
-        
+        dbg!(aspect_ratio);
+
         let bot_prop=BotProp{
             radius:Dist::new(12.0),
             collision_drag:0.003,
@@ -68,8 +28,8 @@ impl BotSystem{
             viscousity_coeff:0.03
         };
 
-        let (bots,mut container_rect) = create_bots(aspect_ratio,num_bots,&bot_prop).unwrap();
-        container_rect.grow(10.0);
+        let (bots,mut container_rect) = create_bots(aspect_ratio,num_bots,&bot_prop);
+        dbg!(container_rect);
         
         
         let mouse_prop=MouseProp{
@@ -94,8 +54,8 @@ impl BotSystem{
     }
 
 
-    pub fn step(&mut self, poses: &[Vec2<f32>],border:&Rect<f32>) {
-        
+    pub fn step(&mut self, poses: &[Vec2<f32>],border:&Vec2<f32>) {
+        let border=rect(0.0,border.x,0.0,border.y);
         let border=border.inner_try_into().unwrap();
 
         {                
@@ -142,18 +102,17 @@ impl BotSystem{
 
 #[derive(Copy,Clone,Debug)]
 pub struct NoBots;
-pub fn create_bots(aspect_ratio:f64,num_bot:usize,bot_prop: &BotProp)->Result<(Vec<Bot>,axgeom::Rect<f32>),NoBots>{
+pub fn create_bots(aspect_ratio:f64,num_bot:usize,bot_prop: &BotProp)->(Vec<Bot>,axgeom::Rect<f32>){
     
-    //let s=dists::spiral::Spiral::new([0.0,0.0],12.0,1.0);
     
-
-    //let s=dists::grid::Grid::new(axgeom::Rect::new(-2000.,2000.,-1300.,1300.),num_bot);
-    //let s=dists::grid::Grid::new(axgeom::Rect::new(-30000.,30000.,-20000.,20000.),num_bot);
     let mut bots=Vec::with_capacity(num_bot);
-    let s=dists::grid::from_center(vec2(0.0,0.0),aspect_ratio as f32,10.0,num_bot,|pos|bots.push(Bot::new(pos)));
+    
+    let end:Vec2<f32>=dists::grid::from_top_start(vec2(0.0,0.0),aspect_ratio as f32,10.0,num_bot,|pos|bots.push(Bot::new(pos)));
+    
+    (bots,rect(0.0,end.x,0.0,end.y))
+    /*
     let bots=bots;
-    //let bots:Vec<Bot>=s.take(num_bot).map(|pos|Bot::new(vec2(pos.x as f32,pos.y as f32))).collect();
-
+    
     let rect=bots.iter().fold(None,|rect:Option<Rect<NotNan<f32>>>,bot|{
         match rect{
             Some(mut rect)=>{
@@ -167,6 +126,7 @@ pub fn create_bots(aspect_ratio:f64,num_bot:usize,bot_prop: &BotProp)->Result<(V
     });
 
 
+    dbg!(rect);
 
     match rect{
         Some(x)=>{
@@ -177,5 +137,6 @@ pub fn create_bots(aspect_ratio:f64,num_bot:usize,bot_prop: &BotProp)->Result<(V
             Err(NoBots)
         }
     }
+    */
 }
 
